6d02bd65ad76363b6ecf16a47e791676bbe0798d
Nicklas Söderberg <nicklas.soderberg@hotmail.se>
2020-11-16 10:54:23 +0100

    Added eslint
    Added eslint

---
+++ whistleblower/src/components/navbar.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="center examplex">
+      <vs-navbar center-collapsed v-model="active">
+        <template #left>
+          <img src="/logo2.png" alt="">
+        </template>
+        <vs-navbar-item :active="active == 'guide'" id="guide">
+          Guide
+        </vs-navbar-item>
+        <vs-navbar-item :active="active == 'docs'" id="docs">
+          Documents
+        </vs-navbar-item>
+        <vs-navbar-item :active="active == 'components'" id="components">
+          Components
+        </vs-navbar-item>
+        <vs-navbar-item :active="active == 'license'" id="license">
+          license
+        </vs-navbar-item>
+        <template #right>
+          <vs-button flat >Login</vs-button>
+          <vs-button>Get Started</vs-button>
+        </template>
+      </vs-navbar>
+      <div class="square">
+        <div class="child">
+          child 1
+        </div>
+        <div class="child">
+          child 2
+        </div>
+        <div class="child">
+          child 3
+        </div>
+      </div>
+    </div>
+  </template>
+  <script>
+  export default {
+    name: 'navbar',
+    data:() => ({
+      active: 'guide'
+    })
+  }
+  </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
