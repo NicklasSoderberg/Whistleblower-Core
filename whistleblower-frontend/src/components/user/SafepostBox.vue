6aab8619a8c0ee6ab38560003dc1ecb37760322d
Skander Damoussi <Damoussi.Skander@gmail.com>
2020-11-22 23:27:47 +0100

    changed message out avatar placing and removed background

--- whistleblower-frontend/src/components/user/SafepostBox.vue
+++ whistleblower-frontend/src/components/user/SafepostBox.vue
@@ -22,7 +22,7 @@
 
 <vs-row>
 
-  <vs-avatar circle size="30" style="margin-left: 90%">
+  <vs-avatar circle size="20" style="margin-left: 90%" class="avatar">
         <template #text>
           W
         </template>
@@ -30,6 +30,20 @@
 
   <p class="message message-out">
       Absolut klockan var 10.
+    </p>
+
+</vs-row>
+
+<vs-row>
+
+  <vs-avatar circle size="20" style="margin-left: 90%" class="avatar">
+        <template #text>
+          W
+        </template>
+</vs-avatar>
+
+  <p class="message message-out">
+      eller klockan var 5.
     </p>
 
 </vs-row>
@@ -75,10 +89,6 @@ width: 100%;
 height: 100%;
   font-family: sans-serif;
   font-weight: 100;
-background: #7b4397;  /* fallback for old browsers */
-background: -webkit-linear-gradient(to right, #fffeff, #8c8b8d);  /* Chrome 10-25, Safari 5.1-6 */
-background: linear-gradient(to right, #fffeff, #8c8b8d);
-/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 margin: 0px;
 }
 
@@ -97,6 +107,11 @@ margin: 0px;
   margin: 0 auto 2em auto;
   box-shadow: 2px 2px 5px 2px rgba(0, 0, 0, 0.3);
   border-radius: 5%;
+}
+.avatar {
+  position: absolute;
+  bottom: 42px;
+  left: 10px;
 }
 .message {
   width: 45%;
