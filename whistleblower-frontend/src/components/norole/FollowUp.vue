8c0b52f051ee491c6c74f374b010b70f8cd4a9f7
André <Andre.roygaard@gmail.com>
2020-12-02 21:54:36 +0100

    Validation för följa upp ärende och login

--- whistleblower-frontend/src/components/norole/FollowUp.vue
+++ whistleblower-frontend/src/components/norole/FollowUp.vue
@@ -4,12 +4,19 @@
       <h3>Ange Ärende ID och lösenord för att följa ditt ärende</h3>
       </vs-row>
       <vs-row id="space" type="flex" justify="center" align="center">
-      <vs-input v-model="value1" placeholder="Ärende ID">
+      <vs-input v-model="value1" v-model.trim="$v.value1.$model" placeholder="Ärende ID">
       </vs-input>
       </vs-row>
+       <vs-row justify="center">
+         <div class="error" v-if="!$v.value1.required" >Ärende ID måste vara ifyllt</div>
+      </vs-row>
       <vs-row type="flex" justify="center" align="center">
-      <vs-input id="space" type="password" icon-after v-model="value2" placeholder="Lösenord">
+      <vs-input id="space" type="password"
+       v-model.trim="$v.value2.$model" icon-after v-model="value2" placeholder="Lösenord">
       </vs-input>
+      </vs-row>
+      <vs-row justify="center">
+         <div class="error" v-if="!$v.value2.required" >Lösenordet måste vara ifyllt</div>
       </vs-row>
       <vs-row id="space" type="flex" justify="center" align="center">
       <vs-button gradient primary>Följ upp ärende</vs-button>
@@ -18,12 +25,22 @@
 </template>
 
 <script>
+import { required } from 'vuelidate/lib/validators';
+
 export default {
   name: 'FollowUp',
   data: () => ({
     value1: '',
     value2: '',
   }),
+  validations: {
+    value1: {
+      required,
+    },
+    value2: {
+      required,
+    },
+  },
 };
 </script>
 
