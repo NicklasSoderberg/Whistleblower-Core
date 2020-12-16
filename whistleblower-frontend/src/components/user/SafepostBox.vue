9272a0add4b65ec5cc59752148e8049d7e34cf21
Nicklas Söderberg <nicklas.soderberg@hotmail.se>
2020-12-16 10:08:09 +0100

    Bilder visas
    Bilder visas

--- whistleblower-frontend/src/components/user/SafepostBox.vue
+++ whistleblower-frontend/src/components/user/SafepostBox.vue
@@ -1,43 +1,74 @@
 <template>
   <body>
-    <h1 style="text-align: center">
-      Välkommen till SafepostBox
-    </h1>
-<section ref="chatArea" class="chat-area">
-<p v-for="conversation in conversations"
-:key="conversation.conversationID" class="message"
-:class="{ 'message-out': conversation.sender === 1, 'message-in': conversation.sender !== 1 }">
-      {{ conversation.message }}
-    </p>
+    <h1 style="text-align: center">Välkommen till SafepostBox</h1>
+    <section ref="chatArea" class="chat-area">
+      <p
+        v-for="conversation in conversations"
+        :key="conversation.conversationID"
+        class="message"
+        :class="{
+          'message-out': conversation.sender === 1,
+          'message-in': conversation.sender !== 1,
+        }"
+      >
+        {{ conversation.message }}
+      </p>
+    </section>
 
-  </section>
-
-  <vs-dialog blur prevent-close v-model="active">
-    <template #header>
-          <h3 class="not-margin">
-            meddelande till juristen.
-          </h3>
-        </template>
-      <form @submit.prevent="sendMessage()" id="person2-form"
-      style="width: 100%; height: 200px;">
-      <textarea v-model="postMessage"
-      id="person2-input" type="text" placeholder="Type your message">
-
-      </textarea>
-<vs-button type="submit" gradient primary @click="
- disableButton === true">Skicka meddelande</vs-button>
-    </form>
-      </vs-dialog>
-      <div justify="center" align="center">
-    <vs-button
+    <vs-dialog blur prevent-close v-model="active">
+      <template #header>
+        <h3 class="not-margin">meddelande till juristen.</h3>
+      </template>
+      <input
+        type="file"
+        id="file"
+        ref="myFiles"
+        accept="image/png, image/jpeg"
+        @change="previewFiles()"
+        multiple
+      />
+      <form
+        @submit.prevent="sendMessage()"
+        id="person2-form"
+        style="width: 100%; height: 200px"
+      >
+        <textarea
+          v-model="postMessage"
+          id="person2-input"
+          type="text"
+          placeholder="Type your message"
+        >
+        </textarea>
+        <vs-button
+          type="submit"
+          gradient
+          primary
+          @click="disableButton === true"
+          >Skicka meddelande</vs-button
+        >
+      </form>
+    </vs-dialog>
+    <div justify="center" align="center">
+      <vs-button
         primary
-        @click="active=!active"
+        @click="active = !active"
         class="btn"
         :disabled="disableButton"
       >
         Svara
       </vs-button>
-  </div>
+      <div v-if="images.length > 0">
+          <img
+            class="images"
+            v-for="(image, i) in images"
+            :key="i"
+            :src="image.base64"
+          />
+        </div>
+        <div v-else>
+          <p>No images added</p>
+        </div>
+    </div>
   </body>
 </template>
 
@@ -46,6 +77,7 @@ import { required } from 'vuelidate/lib/validators';
 import whistle from '../../apicalls/whistle';
 import conversations from '../../apicalls/conversation';
 import mix from '../../mixins/myMixin';
+import files from '../../apicalls/file';
 
 export default {
   name: 'SafepostBox',
@@ -59,6 +91,8 @@ export default {
     postMessage: '',
     timestamp: '',
     mixintime: '',
+    files: [],
+    images: {},
   }),
   validations: {
     postMessage: {
@@ -70,8 +104,7 @@ export default {
       return this.disableButton ? 'disabled' : '';
     },
   },
-  created() {
-  },
+  created() {},
   methods: {
     async createPostMessage() {
       if (this.$v.postMessage.required !== false) {
@@ -85,9 +118,26 @@ export default {
           fileID: 2,
         };
 
-        await conversations.postMessage(this.$store.getters.StateUserToken, messageData);
+        await conversations.postMessage(
+          this.$store.getters.StateUserToken,
+          messageData,
+        );
 
         this.conversations.push(messageData);
+        if (this.files.length > 0) {
+          // eslint-disable-next-line func-names
+          // eslint-disable-next-line no-restricted-syntax
+          for (const file of this.files) {
+            // eslint-disable-next-line no-await-in-loop
+            await files.post(this.$store.getters.StateUserToken, {
+              fileID: 0,
+              // eslint-disable-next-line no-await-in-loop
+              base64: await this.getBase64(file),
+              extension: file.type,
+              whistleID: this.whistle.whistleID,
+            });
+          }
+        }
 
         this.active = !this.active;
       } else {
@@ -95,21 +145,30 @@ export default {
       }
     },
     async getWhistle() {
-      await whistle.getByUserId(this.$store.getters.StateUserToken,
-        this.$store.getters.StateUserId).then((response) => {
-        this.whistle = response;
-      });
+      await whistle
+        .getByUserId(
+          this.$store.getters.StateUserToken,
+          this.$store.getters.StateUserId,
+        )
+        .then((response) => {
+          this.whistle = response;
+        });
     },
     async getConversations(whistleId) {
-      await conversations.getAll(
-        this.$store.getters.StateUserToken,
-        whistleId,
-      ).then((response) => {
-        this.conversations = response;
-        console.log(this.conversations);
-      });
+      await conversations
+        .getAll(this.$store.getters.StateUserToken, whistleId)
+        .then((response) => {
+          this.conversations = response;
+          console.log(this.conversations);
+        });
 
       this.answerDisable();
+    },
+    async getFiles() {
+      await files.get(this.$store.getters.StateUserToken, this.whistle.whistleID)
+        .then((response) => {
+          this.images = response;
+        });
     },
     async sendMessage() {
       await this.createPostMessage();
@@ -125,44 +184,90 @@ export default {
         this.disableButton = false;
       }
     },
+    previewFiles() {
+      this.files = this.$refs.myFiles.files;
+      let fileType;
+      const acceptFiles = ['image/jpg', 'image/jpeg', 'image/png'];
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < this.files.length; i++) {
+        if (this.files[i].size > 5000000) {
+          // eslint-disable-next-line no-alert
+          alert(
+            `${this.files[i].name} is over the 5mb limit!\n\nPlease try again..`,
+          );
+          this.$refs.myFiles.value = '';
+          return;
+        }
+        fileType = this.files[i].type;
+        if (!acceptFiles.includes(fileType)) {
+          // eslint-disable-next-line no-alert
+          alert(
+            `${fileType} is not an supported filetype!\n\nPlease try again..`,
+          );
+          this.$refs.myFiles.value = '';
+          return;
+        }
+      }
+      // eslint-disable-next-line prefer-const
+      let reader = new FileReader();
+      reader.readAsDataURL(this.files[0]);
+      // eslint-disable-next-line func-names
+      // reader.onload = function () {
+      //  console.log(reader.result);
+      // };
+      // eslint-disable-next-line func-names
+      // reader.onerror = function (error) {
+      //  console.log('Error: ', error);
+      // };
+    },
+    async getBase64(file) {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = (error) => reject(error);
+      });
+    },
   },
   async mounted() {
     await this.getWhistle();
     await this.getConversations(this.whistle.whistleID);
+    await this.getFiles();
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-body, html {
-width: 100%;
-height: 100%;
+body,
+html {
+  width: 100%;
+  height: 100%;
   font-family: sans-serif;
   font-weight: 100;
-margin: 0px;
+  margin: 0px;
 }
 textarea {
   resize: none;
-  font-family:"Source Sans Pro", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-family: "Source Sans Pro", "Helvetica Neue", Helvetica, Arial, sans-serif;
   font-size: 16px;
   -webkit-border-radius: 5px;
-    -moz-border-radius: 1px;
-    border-radius: 6px;
-    width: 95%;
-    height: 80%;
-    outline: none;
-    background-color: transparent;
+  -moz-border-radius: 1px;
+  border-radius: 6px;
+  width: 95%;
+  height: 80%;
+  outline: none;
+  background-color: transparent;
 }
 .btn {
-position: relative;
-bottom: 20px;
-max-width: 520px;
-width: 250px;
+  position: relative;
+  bottom: 20px;
+  max-width: 520px;
+  width: 250px;
 }
 .reply-button {
-height: 40px;
-width: 90px;
+  height: 40px;
+  width: 90px;
 }
 .headline {
   text-align: center;
@@ -170,7 +275,7 @@ width: 90px;
   color: white;
 }
 .chat-area {
-/*   border: 1px solid #ccc; */
+  /*   border: 1px solid #ccc; */
   background: white;
   height: 50vh;
   padding: 1em;
@@ -189,31 +294,31 @@ width: 90px;
 }
 .message {
   border-radius: 10px;
-  padding: .5em;
-/*   margin-bottom: .5em; */
+  padding: 0.5em;
+  /*   margin-bottom: .5em; */
 }
 .message-out {
-  background: #407FFF;
+  background: #407fff;
   color: white;
   margin-left: 50%;
   word-wrap: break-word;
   justify-content: right;
 }
 .message-in {
-  background: #F1F0F0;
+  background: #f1f0f0;
   color: black;
   word-wrap: break-word;
-    width: 45%;
+  width: 45%;
 }
 .chat-inputs {
   display: flex;
   justify-content: space-between;
 }
 #person1-input {
-  padding: .5em;
+  padding: 0.5em;
 }
 #person2-input {
-  padding: .5em;
+  padding: 0.5em;
 }
 h1 {
   font-size: 27px;
